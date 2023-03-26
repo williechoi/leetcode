7e617c8008c697c8dab5c26132b5f8432db58d91
Hyungsuk Choi <choihyungsuk@gmail.com>
2023-03-27 00:20:40 +0900

    intersection of two arrays: problem and solution

---
+++ IntersectionOfTwoArrays/intersection_of_two_arrays.py
@@ -0,0 +1,37 @@
+"""
+349. Intersection of Two Arrays
+Given two integer arrays nums1 and nums2,
+return an array of their intersection.
+
+Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+========================================================================
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+========================================================================
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+"""
+
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1).intersection(set(nums2)))
+
+
+if __name__ == "__main__":
+    nums1 = [1, 2, 2, 1]
+    nums2 = [2, 2]
+
+    print(Solution().intersection(nums1, nums2))
+
+    nums1 = [4, 9, 5]
+    nums2 = [9, 4, 9, 8, 4]
+
+    print(Solution().intersection(nums1, nums2))
